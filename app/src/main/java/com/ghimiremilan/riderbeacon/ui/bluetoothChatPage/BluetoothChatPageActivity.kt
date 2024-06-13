d59340e20224dbe2df678e6854dd330a66fd7d0c
Milan Ghimire <milan.ghimire@ensue.us>
2024-06-13 17:29:51 +0545

    Created bluetooth chat page

---
+++ app/src/main/java/com/ghimiremilan/riderbeacon/ui/bluetoothChatPage/BluetoothChatPageActivity.kt
@@ -0,0 +1,15 @@
+package com.ghimiremilan.riderbeacon.ui.bluetoothChatPage
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class BluetoothChatPageActivity: ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+
+        }
+    }
+}
